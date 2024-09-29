0acda05af74eb080329cf81b5949b69359415f19
stanislav.mokhyr <stanislav.mokhyr@idapgroup.com>
2024-09-29 16:21:47 +0300

    change import

--- src/app/api/vocabularyList/route.ts
+++ src/app/api/vocabularyList/route.ts
@@ -1,8 +1,16 @@
-import { SortOrder } from 'mongoose';
+import { Document, SortOrder } from 'mongoose';
 import {NextRequest, NextResponse} from 'next/server';
 import {connectMongoDB} from '@/lib/mongodb';
 import Vocabulary from '@/utils/vocabularySchema/vocabularySchema';
-import { IVocabulary } from '@/models/models';
+
+export interface IVocabulary extends Document {
+  word: string;
+  translate: string;
+  transcription?: string;
+  lable?: string;
+  createdAt: Date;
+  updatedAt: Date;  
+}
 
 type FilterParam = Partial<Record<keyof IVocabulary, any>>;
 type SortParams = Partial<Record<keyof IVocabulary, SortOrder>>;
